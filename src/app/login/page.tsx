788020eb16a307c621a3d2e9540ac7976dfb5fde
Michael <abayojumumichael@gmail.com>
2025-06-19 23:24:40 +0100

    feat: updated login/signup page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,14 +1,60 @@
-import { login, signup } from '@/app/login/actions'
+import { login, signup } from "./actions"
+import {
+    Card,
+    CardAction,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Button } from "@/components/ui/button"
 
 export default function LoginPage() {
-  return (
-    <form>
-      <label htmlFor="email">Email:</label>
-      <input id="email" name="email" type="email" required />
-      <label htmlFor="password">Password:</label>
-      <input id="password" name="password" type="password" required />
-      <button formAction={login}>Log in</button>
-      <button formAction={signup}>Sign up</button>
-    </form>
-  )
+    return (
+        <Card className="w-full max-w-sm">
+            <form>
+                <CardHeader>
+                    <CardTitle>Login to your account</CardTitle>
+                    <CardDescription>
+                        Enter your email below to login to your account
+                    </CardDescription>
+                    <CardAction>
+                        <Button formAction={signup}>Sign Up</Button>
+                    </CardAction>
+                </CardHeader>
+                <CardContent>
+                    <div className="flex flex-col gap-6">
+                        <div className="grid gap-2">
+                            <Label htmlFor="email">Email</Label>
+                            <Input
+                                id="email"
+                                type="email"
+                                name="email"
+                                placeholder="success@example.com"
+                                required
+                            />
+                        </div>
+                        <div className="grid gap-2">
+                            <div className="flex items-center">
+                                <Label htmlFor="password">Password</Label>
+                            </div>
+                            <Input
+                                id="password"
+                                name="password"
+                                type="password"
+                                required
+                            />
+                        </div>
+                    </div>
+
+                </CardContent>
+                <CardFooter className="flex-col gap-2">
+                    <Button formAction={login} className="w-full">Login</Button>
+                </CardFooter>
+            </form>
+        </Card>
+    )
 }
